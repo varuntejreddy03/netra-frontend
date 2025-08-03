190b72fd1d77e5b5d645f1077ff01678660dec84
VarunTej <varuntejreddy03@gmail.com>
2025-08-03 14:36:56 +0530

    changed in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'netrapro',
+  description: 'Created with varun',
+  generator: 'varun',
 }
 
 export default function RootLayout({
